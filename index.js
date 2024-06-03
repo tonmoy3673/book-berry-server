ea08c209c263e479143b5502b262ba6f7e2bc4eb
tonmoy <tonmoy3673@gmail.com>
2024-06-03 14:15:00 +0600

    :lock: connect with MongoDB

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const express = require('express')
+require('dotenv').config();
 const app = express()
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors=require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(express.json())
 app.use(cors())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0twzmk2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("MongoDB is connected");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Server Connected')
