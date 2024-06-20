0ecfd7c5735550fd3251717f8884203fc5e8ffa7
tonmoy <tonmoy3673@gmail.com>
2024-06-21 02:59:32 +0600

    :bulb: set JWT

--- index.js
+++ index.js
@@ -1,44 +1,16 @@
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const port = process.env.PORT || 3000;
 const cors = require("cors");
 const morgan = require("morgan");
-require("dotenv").config();
+const createError = require("http-errors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
-
-
-const verifyJWT = async (req, res, next) => {
-  try {
-    const authHeader = req.headers.authorization;
-    console.log('Authorization Header:', authHeader);
-
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-      throw createError(401, 'Access token not found, Please login, Again!');
-    }
-
-    const token = authHeader.split(' ')[1];
-    console.log('Extracted Token:', token);
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_TOKEN);
-    console.log('Decoded Token:', decoded);
-
-    if (!decoded) {
-      throw createError(401, 'Authorization failed to access data.');
-    }
-
-    req.decoded = decoded;
-    next();
-  } catch (error) {
-    console.error('Token verification error:', error);
-    return next(error);
-  }
-};
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0twzmk2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -50,8 +22,49 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verifyJWT = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      throw createError(401, 'Access token not found, Please login, Again!');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_TOKEN);
+
+    if (!decoded) {
+      throw createError(401, 'Authorization failed to access data.');
+    }
+
+    req.decoded = req.decoded;
+    next();
+  } catch (error) {
+    console.error('Token verification error:', error);
+    return next(error);
+  }
+};
+
+
+// const verifyJWT = (req, res, next) =>{
+//   const authHeader= req.headers.authorization;
+//   if(!authHeader){
+//       return res.status(401).send({message: 'unauthorized access'})
+//   }
+//   const token = authHeader.split(' ')[1];
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) =>{
+//       if(error){
+//           return res.status(403).send({message: 'Forbidden access'})
+//       }
+//       req.decoded = decoded;
+//       next();
+//   })
+// };
+
 async function run() {
   try {
+   
     await client.connect();
     const bookDB = client.db("bookDB");
     const booksCollection = bookDB.collection("booksCollection");
@@ -72,13 +85,21 @@ async function run() {
       const result = await userCollection.updateOne(filter, updateDoc, options);
 
       // JWT
+      // const token = jwt.sign(
+      //   { email: user.email },
+      //   process.env.JWT_SECRET_TOKEN,
+      //   {
+      //     expiresIn: "1d",
+      //   }
+      // );
+
       const token = jwt.sign(
-        { email: user.email },
-        process.env.JWT_SECRET_TOKEN,
-        {
-          expiresIn: "1d",
-        }
-      );
+          email,
+          process.env.JWT_SECRET_TOKEN,
+          {
+            expiresIn: "1d",
+          }
+        );
 
       res.status(200).send({ result, token });
     });
@@ -88,9 +109,11 @@ async function run() {
       const decoded = req.decoded;
       const email = req.params.email;
 
-      if (decoded.email !== email) {
-        res.status(403).send({ message: "unauthorized access" });
-      }
+      console.log(decoded);
+
+      // if (decoded.email !== email) {
+      //   res.status(403).send({ message: "unauthorized access" });
+      // }
       const query = { email: email };
 
       const user = await userCollection.findOne(query);
@@ -98,9 +121,7 @@ async function run() {
     });
 
     app.get("/books", async (req, res) => {
-      const query = {};
-      const cursor = booksCollection.find(query);
-      const books = await cursor.toArray();
+      const books = await booksCollection.find({}).toArray();
       res.send(books);
     });
 
@@ -137,11 +158,15 @@ async function run() {
   } finally {
   }
 }
-run().catch(console.dir);
+run().catch((error)=>console.log(error));
 
 app.get("/", (req, res) => {
   res.send("Server Connected");
 });
+
+// app.use((req,res,next)=>{
+//   res.json({message:'Not Found',path:req.originalUrl})
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
